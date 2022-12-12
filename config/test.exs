683f5fc42543599558c0edff6d150cb50e9f87ea
kirega <joseph.mutiga@gmail.com>
2022-12-12 23:23:35 +0300

    add github action

--- config/test.exs
+++ config/test.exs
@@ -8,7 +8,7 @@ import Config
 config :rando, Rando.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   database: "rando_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: 10
